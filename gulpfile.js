bd69aee49724cc72d3bdebf703708aa3d370dfd4
Ralph Moran <ralphmoran2003@gmail.com>
2018-04-19 00:19:39 -0700

    Added sourcemaps and concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
-var gulp   = require('gulp'       );
-var uglify = require('gulp-uglify');
+var gulp       = require('gulp'            ); // Gulp base
+var uglify     = require('gulp-uglify'     ); // Minify files
+var concat     = require('gulp-concat'     ); // Concat files into a new file
+var sourcemaps = require('gulp-sourcemaps' ); // Load new file in cache/buffer
 
 gulp.task('uglify', function(){
     return gulp.src('app/js/*.js')
+            .pipe(sourcemaps.init())
+            .pipe(concat('main.min.js'))
+            .pipe(sourcemaps.write())
             .pipe(uglify())
             .pipe(gulp.dest('dist/js'));
 });
